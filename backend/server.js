fd63e37d9991542fd329a1a8605fb4d1d697d288
Prathvi <prathvipatil104@gmail.com>
2025-12-10 06:19:10 +0530

    MongoDB connected and env fixed

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import dotenv from "dotenv";
+
+// // Fix __dirname
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Load .env at start
+// dotenv.config({ path: path.resolve(__dirname, ".env") });
 import dotenv from "dotenv";
-
-// Fix __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Load .env at start
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+dotenv.config();
 
 console.log("MongoDB URI:", process.env.MONGODB_URI);
 
